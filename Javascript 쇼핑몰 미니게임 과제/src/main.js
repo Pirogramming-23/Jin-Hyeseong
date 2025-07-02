ebd332892b98afb2f558601ae37dd2b7a85530a2
Jinecastle03 <gptjd0717@korea.ac.kr>
2025-07-02 14:27:22 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -21,10 +21,54 @@ function createHTMLString(item) {
     </li>
     `;
 }
+
+//Handle button click
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;
+    }
+
+    const filtered = items.filter(item => item[key] === value);
+    console.log(filtered);
+    displayItems(filtered);
+
+    // const target = event.target;
+    // const key = target.dataset.key;
+    // const value = target.dataset.value;
+
+    // if (key == null || value == null) {
+    //     return;
+    // }
+    // updateItems(items, key, value);
+}
+
+// //Make the items matching {key: value} invisible
+// function updateItems(items, key, value) {
+//     items.forEach(item => {
+//         if(item.dataset[key]===value) {
+//             item.classList.remove('invisible');
+//         } else {
+//             item.classList.add('invisible');
+//         }
+//     });
+// }
+
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 //main
 loadItems()
     .then(items => {
         displayItems(items);
-        // setEventListeners(items);
+        setEventListeners(items);
     })
-.catch(console.log);
+    .catch(console.log);
