65b0c185b6b0749f203ad0f69db5e8d1fda0a49a
Jinecastle03 <gptjd0717@korea.ac.kr>
2025-07-03 22:44:09 +0900

    배스킨라빈스31 게임🍦 8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -2,8 +2,8 @@ num = 0
 gameOver = False
 winA = False
 
-while(not gameOver):
-    # PlayerA
+def brGame(player):
+    global num, gameOver, winA
     while True:
         user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
@@ -22,37 +22,19 @@ while(not gameOver):
 
     for i in range(n):
         num+=1
-        print("playerA : {0}".format(num))
+        print("player{0} : {1}".format(player, num))
         if num == 31:
             gameOver=True
+            if player == "B":
+                winA = True
             break
-    
+
+
+while(not gameOver):
+    brGame("A")
     if(gameOver): break
+    brGame("B")
 
-    # PlayerB
-    while True:
-        user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-
-        # 정수인지 확인
-        if not user_input.isdigit():
-            print("정수를 입력하세요")
-            continue
-
-        n = int(user_input)
-
-        # 1, 2, 3 중 하나인지 확인
-        if n not in [1,2,3]:
-            print("1,2,3 중 하나를 입력하세요")
-            continue
-        break
-
-    for i in range(n):
-        num+=1
-        print("playerB : {0}".format(num))
-        if num==31:
-            gameOver=True
-            winA=True
-            break
 
 if(winA): print("playerA win!")
 else: print("playerB win!")
