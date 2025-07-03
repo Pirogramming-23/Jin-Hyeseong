9e630ae5e5ea599c7edf2741008809b8a125c3ec
Jinecastle03 <gptjd0717@korea.ac.kr>
2025-07-03 22:27:10 +0900

    배스킨라빈스31 게임🍦 5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -19,3 +19,23 @@ while True:
 for i in range(n):
     num+=1
     print("playerA : {0}".format(num))
+
+while True:
+    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
+    # 정수인지 확인
+    if not user_input.isdigit():
+        print("정수를 입력하세요")
+        continue
+
+    n = int(user_input)
+
+    # 1, 2, 3 중 하나인지 확인
+    if n not in [1,2,3]:
+        print("1,2,3 중 하나를 입력하세요")
+        continue
+    break
+
+for i in range(n):
+    num+=1
+    print("playerB : {0}".format(num))
